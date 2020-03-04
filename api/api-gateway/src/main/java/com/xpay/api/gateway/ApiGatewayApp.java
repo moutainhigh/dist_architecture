bae6930992142eb1fb951db1efcddbdafd1ffae5
chenyf <yufeng629@163.com>
2020-03-04 12:40:07 +0800

    新增文件

--- api/api-gateway/src/main/java/com/xpay/api/gateway/ApiGatewayApp.java
+++ api/api-gateway/src/main/java/com/xpay/api/gateway/ApiGatewayApp.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApp {
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApp.class, args);
     }
